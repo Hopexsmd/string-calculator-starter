71984c4572ca72670a4468ddfaa69ec450301a3f
Sagar Kore <sagarkore13@live.com>
2021-10-16 13:42:30 +0530

    Case 1 empty or null case added

--- String_Calculator/src/main/java/calculator/StringCalculator.java
+++ String_Calculator/src/main/java/calculator/StringCalculator.java
@@ -3,6 +3,12 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	
+    	//Case 1 String(input) is empty or null 
+    	if (input == null || input.length() == 0) {
+			 return 0;
+		}
+    	
         return 0;
     }
 
--- String_Calculator/src/test/java/calculator/StringCalculatorShould.java
+++ String_Calculator/src/test/java/calculator/StringCalculatorShould.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 class StringCalculatorShould {
-
+	//Case 1 Empty string
     @Test
     void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
-
+    
+    //Case 2 String with one input
     @Test
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
